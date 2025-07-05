42f40c1db61966256f4659ec0b15a7de5e442b20
vaibhav chawla <vaibhav.chawla@roundglass.com>
2025-07-05 11:13:57 +0530

    tailwind understanding

--- src/App.jsx
+++ src/App.jsx
@@ -7,29 +7,42 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="grid grid-cols-10 grid-flow-row ">
+      <div className='bg-red-400 col-span-4'>
+        Child 1
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className='bg-green-400'>
+        Child 2
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+
+      <div className='bg-yellow-400'>
+        Child 3        
+      </div>
+
+      <div className='bg-blue-400'>
+        Child 4
+      </div>
+
+
+      <div className='bg-gray-400'>
+        Child 5
+      </div>
+
+
+      <div className='bg-pink-400'>
+        Child 6
+      </div>
+
+
+      <div className='bg-teal-400'>
+        Child 7
+      </div>
+    </div>
   )
 }
 
 export default App
+
+// use un-prefixed utils / classes for mobile design and as the screen-size increases add prefix to your utils / classes (sm -> 640px and above, md -> 768px and above, lg -> 1024px and above , xl -> 1280px and above, 2xl -> 1536px and above ), this is because tailwind approach mobile first approach
